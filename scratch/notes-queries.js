37d4a858ae95c5d178cee270cf413d7b593e5c6c
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-03-13 15:29:06 -0700

    bug fixes

--- scratch/notes-queries.js
+++ scratch/notes-queries.js
@@ -10,45 +10,45 @@ knex.select(1).then(res => console.log(res));
 
 
 
-knex.select('id', 'title', 'content')
-  .from('notes')
-  .where('title', 'like', '%cats%')
-  .then(result => console.log(result));
+// knex.select('id', 'title', 'content')
+//   .from('notes')
+//   .where('title', 'like', '%cats%')
+//   .then(result => console.log(result));
 
 
-knex.select('id', 'title', 'content')
-  .from('notes')
-  .where({'notes.id': '1004'})
-  .then(result => console.log(result));
+// knex.select('id', 'title', 'content')
+//   .from('notes')
+//   .where({'notes.id': '1004'})
+//   .then(result => console.log(result));
 
 
-knex('notes')
-  .update({title:'Lakers cats'})
-  .where({'notes.id': '1004'})
-//   .returning(['id', 'name']) 
-  .then(results => console.log(JSON.stringify(results, null, 2)));
+// knex('notes')
+//   .update({title:'Lakers cats'})
+//   .where({'notes.id': '1004'})
+// //   .returning(['id', 'name']) 
+//   .then(results => console.log(JSON.stringify(results, null, 2)));
 
 
-knex.select('id', 'title', 'content')
-  .from('notes')
-  .where({'notes.id': '1004'})
-  .then(result => console.log(result));
+// knex.select('id', 'title', 'content')
+//   .from('notes')
+//   .where({'notes.id': '1004'})
+//   .then(result => console.log(result));
 
 
-knex
-  .insert({
-    title: 'Vegan cats',
-    content: 'not found. not found. data. data.'
-  })
-  .returning(['id', 'title', 'content'])
-  .into('notes')
-  .then(result => console.log(result));
+// knex
+//   .insert({
+//     title: 'Vegan cats',
+//     content: 'not found. not found. data. data.'
+//   })
+//   .returning(['id', 'title', 'content'])
+//   .into('notes')
+//   .then(result => console.log(result));
 
-knex
-  .del()
-  .from(('notes'))
-  .where({id: 1003})
-  .then(results => console.log(JSON.stringify(results, null, 2)));
+// knex
+//   .del()
+//   .from(('notes'))
+//   .where({id: 1003})
+//   .then(results => console.log(JSON.stringify(results, null, 2)));
 
 
 
